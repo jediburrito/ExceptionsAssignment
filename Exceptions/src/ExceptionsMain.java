0748c49a66bc8f5cc0beab494ce65fce95f40031
jediburrito <jediburrito@gmail.com>
2017-02-02 16:13:54 -0500

    Uploading first project

---
+++ Exceptions/src/ExceptionsMain.java
@@ -0,0 +1,44 @@
+import java.io.FileReader;
+
+public class ExceptionsMain {
+
+	public static void main(String[] args) {
+	
+		 //int [] number = {2,3};
+		//System.out.println(number[3]); 
+		// creates  java.lang.ArrayIndexOutOfBoundsException: Because the array doesn't have a 3rd element
+		
+//		Object zero = new Integer(0) ;
+//		System.out.println((String)zero);
+//		System.out.println(zero);
+	//Creates Class Cast Exception
+		//illegalArgumentException();
+
+		//FileReader fileReader = new FileReader("Bob");
+		//creates filenotfound error
+		
+//			Integer age = Integer.parseInt("Kakarot");
+//	System.out.println(age);	
+//	int[] number = new int[-1];
+	//creates numberFormatException error
+	
+	//nullPointer();
+	//creates both nullpointer error and stackoverflow error BONUS!
+	//	}
+//	
+//	public static void nullPointer(){
+//			ExceptionsMain app = null;
+//			app.nullPointer();
+//			
+//			
+//	}
+	
+	
+//	
+//	public static void illegalArgumentException(){
+//		char[] c = Character.toChars(-1);
+//		System.out.println("Character = " + c[0]);
+//		
+	}
+	
+}
---
+++ Exceptions/src/PapoPepo.java
@@ -0,0 +1,20 @@
+
+public class PapoPepo {
+	private int papo;
+
+	public PapoPepo(int papo) {
+		super();
+		setPapo(papo);
+	}
+
+	public double getPapo() {
+		return papo;
+	}
+
+	public void setPapo(int papo) {
+		this.papo = papo;
+	}
+	
+	
+	
+}
